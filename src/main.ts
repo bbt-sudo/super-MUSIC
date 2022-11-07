d2bd8981d2641e245ad2fad7ea190b40aa3e9026
903308991@qq.com <903308991@qq.com>
2022-11-07 08:53:01 +0800

    添加ui框架

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/intex'
 import { createPinia } from 'pinia'
-import naive from 'naive-ui'
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
 const pinia = createPinia()
-createApp(App).use(router).use(pinia).use(naive).mount('#app')
+createApp(App).use(router).use(pinia).use(ArcoVue).mount('#app')
